edba2bc3afbb8131e5fe07a78085df66bdc89678
Chanpitou <umchanpitou@gmail.com>
2024-11-22 12:40:57 -0600

    first push - chanpitou, model, main

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello World! First Commit")
+import csv
+from model import Data
+import library
+
+# opening the CSV file
+path = "./Customer-Churn-Records.csv"
+# path = input()
+
+with open(path, "r") as file:
+    # reading the CSV file
+    csvFile = csv.reader(file)
+
+    # displaying the contents of the CSV file
+    count = 0
+    entries_list = []
+    for lines in csvFile:
+        # BREAK. taking only 200 for now
+        count += 1
+        if count >= 200:
+            break
+
+        entry = Data(*lines)
+        entries_list.append(entry)
+
+    for entry in entries_list:
+        print(entry)
