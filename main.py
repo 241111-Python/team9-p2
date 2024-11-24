e2b0a6394b4f8638ef17425b9e691dcd88fec9aa
Chanpitou <umchanpitou@gmail.com>
2024-11-23 22:30:27 -0600

    Added pagination functionality

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from library import printEntries, csvRead
 
 
 def main():
-    # Generates command line text menu
-    print("Welcome to the Bank Churn Data Analysis Menu")
-    print("--------------------------------------------")
-    print("1. Display Provided Dataset: (Pagination...)")
-    print("2. Load CSV Dataset")
-    print("3. Analysis")
-    print("4. Exit")
-    print("--------------------------------------------")
-
     # Mantains application running
     while True:
-        choice = input("Please choose between 1-2: ")
+        # Generates command line text menu
+        print("Welcome to the Bank Churn Data Analysis Menu")
+        print("--------------------------------------------")
+        print("1. Display Provided Dataset: (Pagination...)")
+        print("2. Load CSV Dataset")
+        print("3. Analysis")
+        print("4. Exit")
+        print("--------------------------------------------")
+
+        choice = input("Please choose between 1-4: ")
         try:
             number = int(choice)
             if number >= 1 and number < 3:
